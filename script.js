c717fd8a64f1d02358933d70e9655d99ee29ef5d
Diêgo Sousa <34814235+DiegoSousa123@users.noreply.github.com>
2023-11-29 14:10:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,150 +4,155 @@ releve os erros bobos que podem haver.
 E sim, está uma bagunça!
 */
 
-var randNum = parseInt(Math.random() * 1001 + 1);
+var randNum
 var res;
 var iValue;
 var nTentativa = 0;
-/*var state = false;*/
-var padraoEffect = "rgb(0,0,205)";
-var errouEffect = "rgb(255,10,10)";
-var acertouEffect = "rgb(15,225,96)";
-var acertouDePrima = "rgb(255, 164, 33)";
+var root = document.documentElement;
 
-document.getElementById("n_tentativa").innerHTML =
-    "<small>Tentativas: " + nTentativa + "</small>";
-document.getElementById("back_effect").style.animation =
-    "effect 1.5s linear infinite";
+var padraoEffect = getComputedStyle(root).getPropertyValue("--cor_padrao");
+var errouEffect = getComputedStyle(root).getPropertyValue("--cor_errou");
+var acertouEffect = getComputedStyle(root).getPropertyValue("--cor_acertou");
+var acertouDePrima = getComputedStyle(root).getPropertyValue("--cor_lendario");
+
+document.getElementById("n_tentativa").innerHTML = "<small>Tentativas: " + nTentativa + "</small>";
+document.getElementById("back_effect").style.animation = "effect 1.5s linear infinite";
 document.getElementById("resultado").style.opacity = "0";
 
-console.log(randNum);
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("preload", "auto");
+audioElement.setAttribute("controls", "none");
+audioElement.setAttribute("hidden", "true");
+document.body.appendChild(audioElement);
 
-var objAudio = document.createElement("audio");
-objAudio.setAttribute("preload","auto");
-objAudio.setAttribute("controls","none");
-objAudio.setAttribute("hidden", "true");
-document.body.appendChild(objAudio);
-
+rand();
 
 function chute() {
-    iValue = parseInt(document.getElementById("input_n").value);
-    var result = document.getElementById("resultado");
-    var tentativa = document.getElementById("n_tentativa");
-    var maiorMenor = document.getElementById("maior_menor");
+	iValue = parseInt(document.getElementById("input_n").value);
+	var result = document.getElementById("resultado");
+	var tentativa = document.getElementById("n_tentativa");
+	var maiorMenor = document.getElementById("maior_menor");
 
-    if (iValue > 0) {
-        if (iValue > 1000) {
-            result.innerHTML = "números de 1 a <ins>1000</ins>";
-            document.getElementById("resultado").style.opacity = "1";
-            document.getElementById("resultado").style.color = "#f9f9f9";
-            document.getElementById("input_n").value = "";
-            setTimeout(function () {
-                result.innerHTML = "";
-            }, 1500);
-        } else if (iValue == randNum) {
-            if (nTentativa == 0) {
-                //acertou na primeira tentativa
-                result.innerHTML = "LENDÁRIO!";
-                document.getElementById("back_effect").style.background =
-                    acertouDePrima;
-                document.getElementById("resultado").style.color =
-                    acertouDePrima;
-                maiorMenor.innerHTML = "<small>acertou de primeira!</small>";
-            } else {
-                document.getElementById("back_effect").style.background =
-                    acertouEffect;
-                document.getElementById("resultado").style.color =
-                    "rgb(15,225,96)";
-                result.innerHTML = "ACERTOU";
-            }
-            toque("acertou");
-            document.getElementById("resultado").style.opacity = "1";
-            blockInputs(true);
-        } else if (iValue != randNum) {
-            ++nTentativa;
-            document.getElementById("n_tentativa").innerHTML =
-                "<small>Tentativas: " + nTentativa + "</small>";
-            document.getElementById("resultado").style.opacity = "1";
-            result.innerHTML = "ERROU";
-            toque("errou");
-            document.getElementById("input_n").disabled = true;
-            document.getElementById("resultado").style.color = "red";
-            document.getElementById("chute_btn").disabled = true;
-            document.getElementById("back_effect").style.background =
-                errouEffect;
+	if (iValue > 0) {//verifica se o valor é maior que 0
+		if (iValue > 1000) {//verifica se o valor é maior que 1000
+			result.innerHTML = "números de 1 a <ins>1000</ins>";
+			document.getElementById("resultado").style.opacity = "1";
+			document.getElementById("resultado").style.color = "#f9f9f9";
+			document.getElementById("input_n").value = "";
+			setTimeout(function () {
+				result.innerHTML = "";
+			}, 1500);
+		} else if (iValue == randNum) { //verifica se o valor é igual ao numero aleatorio
+			if (nTentativa == 0) {
+				//acertou na primeira tentativa
+				result.innerHTML = "LENDÁRIO!";
+				document.getElementById("back_effect").style.background = acertouDePrima;
+				document.getElementById("resultado").style.color = acertouDePrima;
+				maiorMenor.innerHTML = "<small>acertou de primeira!</small>";
+			} else {
+				//acertou
+				document.getElementById("back_effect").style.background = acertouEffect;
+				document.getElementById("resultado").style.color = acertouEffect;
+				result.innerHTML = "ACERTOU";
+			}
+			toque("acertou"); //realiza o toque de acerto
+			document.getElementById("resultado").style.opacity = "1";
+			blockInputs(true); //bloqueia os botões e input
+		} else if (iValue != randNum) { //verifica se o valor é diferente (errou)
+			++nTentativa; //incrementa o contador de tentativas
+			// saveChute(iValue); //salvar e exibir chute
+			document.getElementById("n_tentativa").innerHTML =
+				"<small>Tentativas: " + nTentativa + "</small>";
+			document.getElementById("resultado").style.opacity = "1";
+			result.innerHTML = "ERROU";
+			document.getElementById("input_n").disabled = true;
+			document.getElementById("resultado").style.color = errouEffect;
+			toque("errou");//realiza o toque de errou
+			document.getElementById("chute_btn").disabled = true;
+			document.getElementById("back_effect").style.background = errouEffect;
 
-            setTimeout(function () {
-                if (nTentativa == 10) {
-                    result.innerHTML = "<b>Tentativas esgotadas!</b>";
-                    document.getElementById("resultado").style.opacity = "1";
-                    blockInputs(true);
-                    maiorMenor.innerHTML =
-                        "<small>O número era: " + randNum + "</small>";
-                } else {
-                    result.innerHTML = "";
-                    document.getElementById("input_n").value = "";
-                    document.getElementById("input_n").disabled = false;
-                    maiorMenor.innerHTML = "";
-                    document.getElementById("resultado").style.opacity = "0";
-                    document.getElementById("chute_btn").disabled = false;
-                    document.getElementById("back_effect").style.background =
-                        padraoEffect;
-                }
-            }, 2000);
+			setTimeout(function () {
+				if (nTentativa == 10) {//verifica se as tentativas atingiram o limite
+					result.innerHTML = "<b>tentativas esgotadas!</b>";
+					document.getElementById("input_n").value = "";
+					document.getElementById("resultado").style.opacity = "1";
+					blockInputs(true);
+					maiorMenor.innerHTML = "<small>o número era: " + randNum + "</small>";
+				} else {
+					result.innerHTML = "";
+					document.getElementById("input_n").value = "";
+					document.getElementById("input_n").disabled = false;
+					maiorMenor.innerHTML = "";
+					document.getElementById("resultado").style.opacity = "0";
+					document.getElementById("chute_btn").disabled = false;
+					document.getElementById("back_effect").style.background = padraoEffect;
+				}
+			}, 2000);
 
-            if (iValue > randNum) {
-                maiorMenor.innerHTML = "<small>é menor...</small>";
-            }
-            if (iValue < randNum) {
-                maiorMenor.innerHTML = "<small>é maior...</small>";
-            }
-        }
-    }else{
-						var inputBox = document.getElementById("input_n");
-            inputBox.classList.toggle("emptyUserInput");
-            setTimeout(function(){
-            	inputBox.classList.toggle("emptyUserInput");
-            }, 700);
-    }
-}
-
-function blockInputs(state) {
-    if (state == true) {
-        document.getElementById("input_n").disabled = true;
-        document.getElementById("chute_btn").disabled = true;
-        document.getElementById("restart").disabled = false;
-    } else {
-    }
-}
-function rand() {
-    randNum = parseInt(Math.random() * 1001 + 1);
-    console.log(randNum);
-}
-function restart() {
-    nTentativa = 0;
-    document.getElementById("input_n").value = "";
-    document.getElementById("resultado").innerHTML = "";
-    document.getElementById("resultado").style.opacity = "0";
-    document.getElementById("n_tentativa").innerHTML =
-        "<small>Tentativas: " + nTentativa + "</small>";
-    document.getElementById("maior_menor").innerHTML = "";
-    document.getElementById("input_n").disabled = false;
-    document.getElementById("chute_btn").disabled = false;
-    document.getElementById("restart").disabled = true;
-    document.getElementById("back_effect").style.background = padraoEffect;
-
-    rand();
-}
-
-//função para tocar o audio relacionado a condição.
-function toque(condicao){
-	objAudio.volume = 0.8;
-	if(condicao === "acertou"){
-		objAudio.src = "win.mp3";
-	}else{
-		objAudio.volume = 0.3;
-		objAudio.src = "fail.wav";
+			if (iValue > randNum) {
+				maiorMenor.innerHTML = "<small>é menor...</small>";
+			}
+			if (iValue < randNum) {
+				maiorMenor.innerHTML = "<small>é maior...</small>";
+			}
+		}
+	} else {//valor menor ou igual a zero 
+		var inputBox = document.getElementById("input_n");
+		inputBox.classList.toggle("emptyUserInput");
+		setTimeout(function () {
+			inputBox.classList.toggle("emptyUserInput");
+		}, 700);
 	}
-	objAudio.load();
-	objAudio.play();
 }
+
+function blockInputs(state) {//função para bloquear os elementos
+	if (state == true) {
+		document.getElementById("input_n").disabled = true;
+		document.getElementById("chute_btn").disabled = true;
+		document.getElementById("restart").disabled = false;
+	} else {
+	}
+}
+function rand() {//função para gerar o numero aleatorio
+	randNum = parseInt(Math.random() * 1001 + 1);
+	if (randNum === 1001) {
+		randNum = parseInt(Math.random() * 1001 + 1);
+	}
+	console.log(randNum);
+}
+function restart() {//função para reiniciar
+	nTentativa = 0;
+	document.getElementById("input_n").value = "";
+	document.getElementById("resultado").innerHTML = "";
+	document.getElementById("resultado").style.opacity = "0";
+	document.getElementById("n_tentativa").innerHTML =
+		"<small>Tentativas: " + nTentativa + "</small>";
+	document.getElementById("maior_menor").innerHTML = "";
+	document.getElementById("input_n").disabled = false;
+	document.getElementById("chute_btn").disabled = false;
+	document.getElementById("restart").disabled = true;
+	document.getElementById("back_effect").style.background = padraoEffect;
+	// saveChute("clear");
+	rand();
+}
+
+function toque(condicao) {//função para tocar o audio
+	audioElement.volume = 1;
+	if (condicao === "acertou") {
+		audioElement.src = "win.mp3";
+	} else {
+		audioElement.volume = 0.1;
+		audioElement.src = "fail.wav";
+	}
+	audioElement.load();
+	audioElement.play();
+}
+
+// function saveChute(input) {
+// 	var chuteObj = document.querySelector(".section__chutes__chutes");
+// 	if (input === "clear") {
+// 		chuteObj.innerHTML = "";
+// 	} else {
+// 		chuteObj.innerHTML += input + " ";
+// 	}
+// }
